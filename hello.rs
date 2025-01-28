2f3079f51ebf9e4b6190d350b3c631bc400f6ad8
Asad Memon <asadmemon@google.com>
2025-01-28 10:54:00 -0800

    loop test

--- hello.rs
+++ hello.rs
@@ -29,9 +29,62 @@ fn match_test(val:i32){
     }
 }
 
+fn loop_test(){
+    for x in [1,2,3,4]{
+        println!("loop test {}", x);
+    }
+}
+
+fn inf_loop(){
+    let mut x = 5;
+    loop{
+        println!("hello world");
+        x-=1;
+        if x==0{
+            break;
+        }
+    }
+}
+
+fn loop_as_expression(){
+    let mut x = 5;
+    let breakswith = loop {
+        println!("loop_as_expression::inside loop");
+        x-=1;
+        if x==0{
+            break x;
+        }
+    };
+    println!("breakswith {breakswith}"); // println can have a var directly inside it
+}
+
+fn loop_label_break(){
+    let mut x = 5;
+    'outer: loop {
+        println!("outer");
+        loop {
+            println!("inner");
+            x-=1;
+            if x==0{
+                break 'outer;
+            }
+        }
+    }
+}
+
+fn debug_print(){
+    let arr = [1,2,3,4];
+    println!("{:?}", arr);
+}
+
 fn main(){
     mutation_test(); // fails if `mut` is not present
     // overflow_test(); // panic: attempt to add with overflow
     no_ternary();
     match_test(7);
+    loop_test();
+    inf_loop();
+    loop_as_expression();
+    loop_label_break();
+    debug_print();
 }
