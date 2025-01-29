0f21acf55b5d853746f0cc578c543ad6f96d5607
Asad Memon <asadmemon@google.com>
2025-01-29 10:30:56 -0800

    trait

--- day2.rs
+++ day2.rs
@@ -7,8 +7,8 @@ struct Car {
 
 impl Car{
     // no constructors in rust, rust has factory methods
-    fn new(name: String, color: String, health: i32) -> Car{
-        Car{name, color, health}
+    fn new(name: String, color: String, health: i32) -> Self{
+        Self{name, color, health}
     }
 
     fn change_health(&mut self, health: i32){
@@ -23,6 +23,37 @@ fn methods_test(){
     println!("{:?}", car);
 }
 
+trait Human {
+    fn talk(&self) -> String;
+    fn walk(&self, x: i32, y: i32) -> String;
+}
+
+struct Asad{
+    name: String,
+    age: i32,
+}
+
+impl Human for Asad{
+    fn talk(&self) -> String {
+        "Hello".to_string()
+    }
+
+    fn walk(&self, x: i32, y: i32) -> String {
+        format!("Walking to ({x}, {y})")
+    }
+}
+
+fn trait_test(){
+    let asad = Asad{
+        name: String::from("Asad"),
+        age: 27,
+    };
+    println!("{}", asad.talk());
+    println!("{}", asad.walk(1,2));
+}
+
+
 fn main(){
     methods_test();
+    trait_test();
 }
