e16cb3845ee3cc23ca3bba0aef794b7967f5dc15
Asad Memon <asadmemon@google.com>
2025-02-05 15:41:36 -0800

    exercise: ffi wrapper using unsafe rust

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-# rustc -g -o app protobuf_parsing.rs && ./app
-rustc --test -g -o app expressioneval_results.rs && ./app
+rustc -g -o app ffi_wrapper.rs && ./app
+# rustc --test -g -o app expressioneval_results.rs && ./app
