3f0872a995435b14bcdcef4fabe83e7d773b6479
Asad Memon <asadmemon@google.com>
2025-02-05 11:44:05 -0800

    exercise: gui modules structuring

---
+++ gui-modules/src/main.rs
@@ -0,0 +1,17 @@
+mod widget;
+mod label;
+mod button;
+mod window;
+
+use widget::Widget;
+use label::Label;
+use button::Button;
+use window::Window;
+
+
+fn main() {
+    let mut window = Window::new("Rust GUI Demo 1.23");
+    window.add_widget(Box::new(Label::new("This is a small text GUI demo.")));
+    window.add_widget(Box::new(Button::new("Click me!")));
+    window.draw();
+}
